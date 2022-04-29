cd091fcefe9d92d61de923a1c7bd2bd1049e3707
neomq <neo.minqi@trentglobal.edu.sg>
2022-04-29 02:55:29 +0000

    updated database.json to use env variables

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -1,12 +1,12 @@
 // Setting up the database connection
 const knex = require('knex')({
-    client: 'mysql',
-    connection: {
-      user: 'neomq',
-      password:'Lazyapple123',
-      database:'essential_oils'
-    }
-  })
-  const bookshelf = require('bookshelf')(knex)
+  client: process.env.DB_DRIVER,
+  connection: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  }
+})
+const bookshelf = require('bookshelf')(knex)
   
   module.exports = bookshelf;
