218f94208c2453e9de860c3037607a7ddb865b9e
neomq <neo.minqi@trentglobal.edu.sg>
2022-05-02 05:55:20 +0000

    disable csrf for api routes, added jwt for users

--- index.js
+++ index.js
@@ -53,22 +53,41 @@ app.use(function(req,res,next){
 })
 
 // enable CSRF
-app.use(csrf());
+// app.use(csrf());
+const csurfInstance = csrf();  // creating a prox of the middleware
+app.use(function(req,res,next){
+  // Disable CSRF for all urls beginning with '/api/'
+  // if the url is for the api, exclude from csrf
+  // if it is webhook url, then call next() immediately
+  if (req.url === '/checkout/process_payment' || 
+      req.url.slice(0,5)=='/api/') {
+      next();
+  } else {
+      csurfInstance(req,res,next);
+  }
+})
 
+// middleware to share the csrf token with all hbs files
+app.use(function(req,res,next){
+  // the req.csrfToken() generates a new token
+  // and save its to the current session
+  if (req.csrfToken) {
+      res.locals.csrfToken = req.csrfToken();
+  }
+  next();
+})
+
+// middleware to handle csrf errors
+// if a middleware function takes 4 arguments
+// the first argument is error
 app.use(function (err, req, res, next) {
   if (err && err.code == "EBADCSRFTOKEN") {
       req.flash('error_messages', 'The form has expired. Please try again');
-      res.redirect('back');
+      res.redirect('back'); //go back one page
   } else {
-      next()
+      next();
   }
 });
-
-// Share CSRF with hbs files
-app.use(function(req,res,next){
-  res.locals.csrfToken = req.csrfToken();
-  next();
-})
 
 // import in routes
 const landingRoutes = require('./routes/landing');
@@ -78,7 +97,8 @@ const userRoutes = require('./routes/users');
 
 // API routes
 const api = {
-  products: require('./routes/api/products')
+  products: require('./routes/api/products'),
+  users: require('./routes/api/users')
 }
 
 async function main() {
@@ -86,7 +106,9 @@ async function main() {
     app.use('/products', productRoutes);
     app.use('/cloudinary', cloudinaryRoutes);
     app.use('/users', userRoutes);
-    app.use('/api/products', api.products);
+
+    app.use('/api/products', express.json(), api.products); 
+    app.use('/api/users', express.json(), api.users); 
 }
 
 main();
--- routes/api/products.js
+++ routes/api/products.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const productDataLayer = require('../../dal/products')
 
-router.get('/', async(req,res)=>{
+router.get('/', async (req,res) => {
     res.send(await productDataLayer.getAllProducts())
 })
 
