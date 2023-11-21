0b7b268e0d43c6938b83aa53e0be3c124355957f
Marcus <97861729+MarcusCWD@users.noreply.github.com>
2023-11-21 21:06:40 +0800

    updated db connection pool settings

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -5,11 +5,9 @@ const knex = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    ssl: {
-      'rejectUnauthorized': false
-    }
-  }
+    host: process.env.DB_HOST
+  },
+  pool: {min: 0, max: 7}
 })
 const bookshelf = require('bookshelf')(knex)
   
