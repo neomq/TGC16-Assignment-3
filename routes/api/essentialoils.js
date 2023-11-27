8168b60882b0303c59180e11042ec66170766114
Neo Min Qi <97939518+neomq@users.noreply.github.com>
2023-11-27 12:23:14 +0800

    add price in sgd

--- routes/api/essentialoils.js
+++ routes/api/essentialoils.js
@@ -1,14 +1,21 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 
 const productDataLayer = require('../../dal/products')
 
 // get essential oil by id
 router.get('/:eo_id', async (req, res) => {
     const essentialOilsId = req.params.eo_id
-    const eachEssentialOil = await productDataLayer.getEssentialOilByID(essentialOilsId);
+    const eachEssentialOil = await productDataLayer.getEssentialOilByID(essentialOilsId)
 
-    res.send(eachEssentialOil)
+    let eoProduct = eachEssentialOil.toJSON()
+
+    // display product price in SGD
+    eoProduct.products.forEach(product => (
+        product.price_sgd = (product.price / 100).toFixed(2)
+    ))
+
+    res.send(eoProduct)
 })
 
 module.exports = router;
